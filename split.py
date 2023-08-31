0895f258a454632a6719b00cc5a56e1296a7744a
Lyx_xjtu <1115137437@qq.com>
2023-08-31 10:05:27 +0800

    Update split.py

--- split.py
+++ split.py
@@ -16,8 +16,8 @@ def saveDfList(dimmMap, dfList):
         sn = sub[0]
         if deviceId != 'cpu0' and deviceId != 'cpu1' and deviceId != 'cpu2' and deviceId != 'cpu3':
             continue
-        elif sn not in dimmMap:
-            continue
+        # elif sn not in dimmMap:
+        #     continue
         subPath = os.path.join(SPLIT_DATA_PATH, sn)
         if not os.path.exists(subPath):
             os.makedirs(subPath)
@@ -25,7 +25,30 @@ def saveDfList(dimmMap, dfList):
         subdf.to_csv(errorFile, index=False)
     
 
-
+def countData(dfList):
+    uceDIMMCount = 0
+    ceDIMMCount = 0
+    unpredictableUceDIMM = 0
+    for sub in dfList:
+        subDf = sub[1].reset_index(drop= True)
+        
+        ceFlag = False
+        uceFlag = False
+        unpredictableFlag = False
+        for _, error in subDf.iterrows():
+            if error['err_type'] == "CE":
+                ceFlag = True
+            else:
+                uceFlag = True
+                if not ceFlag:
+                    unpredictableFlag = True
+        if ceFlag:
+            ceDIMMCount += 1
+        if uceFlag:
+            uceDIMMCount += 1
+        if unpredictableFlag:
+            unpredictableUceDIMM += 1
+    print("ce DIMM:{}, uce DIMM:{}, unpredictable uce DIMM:{}".format(ceDIMMCount, uceDIMMCount, unpredictableUceDIMM))
     
 
 # 按dimm sn号切分数据，并解析
@@ -52,6 +75,7 @@ def splitByDIMM():
     df = pd.json_normalize(data)
     df = df.rename(columns={'phy_addr.bank':'bank' ,'phy_addr.bankgroup': 'bankgroup','phy_addr.column':'column' ,'phy_addr.device':'device' ,'phy_addr.rank':'rank' ,'phy_addr.row':'row'})
     dfList = list(df.groupby('dimm_sn'))
+    countData(dfList)
     processList = []
     cpuCount = os.cpu_count() * 2
     subListSize = math.ceil(len(dfList) / cpuCount)
